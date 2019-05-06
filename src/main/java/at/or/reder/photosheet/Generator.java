496052a89896e648f7117a265b44a43626284b87
reder <w.reder@mountain-sd.at>
2019-05-06 14:31:48 +0200

    version 1.1: leichteres Gitter

--- src/main/java/at/or/reder/photosheet/Generator.java
+++ src/main/java/at/or/reder/photosheet/Generator.java
@@ -1,7 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2019 Wolfgang Reder.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package at.or.reder.photosheet;
 
@@ -223,7 +233,7 @@ public final class Generator
 
   private static String loadSWVersion()
   {
-    URL u = Generator.class.getResource("/META-INF/maven/com.reder/Photosheet/pom.properties");
+    URL u = Generator.class.getResource("/META-INF/maven/at.or.reder/photosheet/pom.properties");
     if (u != null) {
       try (InputStream is = u.openStream()) {
         Properties props = new Properties();
